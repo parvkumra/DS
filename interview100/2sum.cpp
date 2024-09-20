4c54af31ec28dc62a86ec0c196f89ca41151ecda
Parv Kumra <parvkumra2003@gmail.com>
2024-09-20 22:24:29 +0530

    two sum

---
+++ interview100/2sum.cpp
@@ -0,0 +1,15 @@
+#include<vector>
+#include<map>
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       map<int,int>mp;
+
+   for(int i=0;i<nums.size();i++){
+    int new_target=target-nums[i];
+    if(mp.find(new_target)!=mp.end()) return {i,mp[new_target]};
+    else mp[nums[i]]=i;
+   }
+   return {};
+    }
+};
