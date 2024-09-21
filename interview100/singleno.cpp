1a72aed8d55840e9820d8ce8ce3be41030d627d9
Parv Kumra <parvkumra2003@gmail.com>
2024-09-22 01:30:49 +0530

    single number

---
+++ interview100/singleno.cpp
@@ -0,0 +1,17 @@
+#include<vector>
+#include<map>
+using namespace std;
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int,int>mp;
+
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        for(auto it=mp.begin();it!=mp.end();it++){
+            if(it->second==1)return it->first;
+        }
+        return -1;
+    }
+};
