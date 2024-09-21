9fd311526142a38b56f8996c657b1543cba47d7f
Parv Kumra <parvkumra2003@gmail.com>
2024-09-22 01:17:51 +0530

    linked list cycle

---
+++ interview100/llcycle.cpp
@@ -0,0 +1,19 @@
+
+  struct ListNode {
+      int val;
+      ListNode *next;
+  };
+ 
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+       ListNode* slow=head;
+       ListNode* fast=head;
+       while(fast!=nullptr && fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)return true;
+       } 
+       return false;
+    }
+};
