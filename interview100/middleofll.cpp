ab3bbc22bee9d9fecbdc23632d4d83d8fa3acb69
Parv Kumra <parvkumra2003@gmail.com>
2024-09-23 21:35:41 +0530

    middle of ll

---
+++ interview100/middleofll.cpp
@@ -0,0 +1,20 @@
+
+struct ListNode {
+      int val;
+      ListNode *next;
+     ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=nullptr && fast->next!=nullptr ){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+};
