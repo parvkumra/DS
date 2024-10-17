7aba42c938507df1c3277c9b18abfb0028b9929b
Parv Kumra <parvkumra2003@gmail.com>
2024-10-17 11:03:58 +0530

    add linked list

---
+++ interview100/addNumber.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummynode = new ListNode(0);
+        ListNode* curr = dummynode;
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr || carry) {
+            int sum = carry;
+            if (l1 != nullptr) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != nullptr) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+        }
+
+        return dummynode->next;
+    }
+};
